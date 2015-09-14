1ec6e6af15f41209089ee8181f09c6f6379447a7
Jade Shan <evokeralucard@gmail.com>
2015-09-15 00:23:22 +0800

    split dao and mongo

--- jadeutils-mongo/src/test/java/testsuite/AllTestSuite.java
+++ jadeutils-mongo/src/test/java/testsuite/AllTestSuite.java
@@ -1,12 +1,12 @@
 package testsuite;
 
-import jadeutils.dao.JadeDaoTest;
+import jadeutils.mongo.UserDaoTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ UnitTestSuite.class, JadeDaoTest.class })
+@Suite.SuiteClasses({ UnitTestSuite.class, UserDaoTest.class })
 public class AllTestSuite {
 
 }
