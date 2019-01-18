7616476f366882b61e7736d4262e2e584052b72f
Jade Shan <evokeralucard@gmail.com>
2019-01-19 01:11:54 +0800

    update

--- javautils-base/src/test/java/jadeutils/reflect/BeanUtilsTest.java
+++ javautils-base/src/test/java/jadeutils/reflect/BeanUtilsTest.java
@@ -4,7 +4,7 @@
 package jadeutils.reflect;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import jadeutils.base.BeanUtils;
