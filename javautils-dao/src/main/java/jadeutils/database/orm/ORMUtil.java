9e4143f4937fe1fa00534737d9ea352bd7423013
Jade Shan <evokeralucard@gmail.com>
2020-04-12 19:49:54 +0800

    orm tool

--- javautils-dao/src/main/java/jadeutils/database/orm/ORMUtil.java
+++ javautils-dao/src/main/java/jadeutils/database/orm/ORMUtil.java
@@ -5,35 +5,52 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 
 public class ORMUtil {
 	
-	public static <T extends Record<K>, K> T result2record(Class<T> clazz, ResultSet rs) //
+	public static Map<String, Object> result2map(Set<String> colNames, ResultSet rs) {
+		Map<String, Object> record = new HashMap<>();
+		for (String colName : colNames) {
+			try {
+				record.put(colName, rs.getObject(colName));
+			} catch (SQLException e) { e.printStackTrace(); }
+		}
+		return record;
+	}
+
+	public static <T extends Record<K>, K> T result2record(Class<T> clazz, Set<String> colNames, ResultSet rs) //
 			throws InstantiationException, IllegalAccessException, IllegalArgumentException, //
-			InvocationTargetException, NoSuchMethodException, SecurityException  //
+			InvocationTargetException, NoSuchMethodException, SecurityException //
 	{
 		T record = null;
 		if (null != rs) {
 			record = clazz.getDeclaredConstructor(clazz).newInstance();
 			Field[] fields = clazz.getDeclaredFields();
-			for (Field f: fields) {
+			for (Field f : fields) {
 				Column clm = f.getAnnotation(Column.class);
 				if (clm != null) {
 					String colName = clm.column();
 					if (StringUtils.isBlank(colName)) {
 						colName = f.getName();
 					}
-					try {
-						int colIdx = rs.findColumn(colName);
-						Object colValue = rs.getObject(colIdx);
-						f.setAccessible(true);
-						f.set(record, colValue);
-					} catch (SQLException | IllegalArgumentException | //
-							IllegalAccessException e) //
-					{
-						e.printStackTrace();
+					if (null != colNames && !colNames.isEmpty() && !colNames.contains(colName)) {
+						// skip this column
+					} else {
+						try {
+							int colIdx = rs.findColumn(colName);
+							Object colValue = rs.getObject(colIdx);
+							f.setAccessible(true);
+							f.set(record, colValue);
+						} catch (SQLException | IllegalArgumentException | //
+								IllegalAccessException e) //
+						{
+							e.printStackTrace();
+						}
 					}
 				}
 			}
