24cda76796ec9f11f40949c16c018e8b0183a6d6
Jade Shan <evokeralucard@gmail.com>
2020-04-21 16:45:44 +0800

    json util

--- javautils-base/src/test/java/net/jadedungeon/javautil/testsuit/UnitTestSuite.java
+++ javautils-base/src/test/java/net/jadedungeon/javautil/testsuit/UnitTestSuite.java
@@ -1,16 +1,16 @@
 package net.jadedungeon.javautil.testsuit;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import net.jadedungeon.javautil.base.BinaryCoderTest;
 import net.jadedungeon.javautil.base.MD5CoderTest;
 import net.jadedungeon.javautil.datastructures.TreeNodeTest;
 import net.jadedungeon.javautil.image.ImageUtilsTest;
-import net.jadedungeon.javautil.json.JsonUtilsTest;
 import net.jadedungeon.javautil.misc.MessageMapTest;
 import net.jadedungeon.javautil.misc.RegexTest;
 import net.jadedungeon.javautil.reflect.BeanUtilsTest;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import net.jadedungeon.javautil.text.JsonUtilsTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ BinaryCoderTest.class, MD5CoderTest.class,
